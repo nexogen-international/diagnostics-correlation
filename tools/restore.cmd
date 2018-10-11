0e3cf2a28ab05b235b171be3527ee04c16ddbf88
Adam Hoka <hoka.adam@nexogen.hu>
2018-10-11 14:32:01 +0200

    Clean up

--- tools/restore.cmd
+++
@@ -1,5 +0,0 @@
-@ECHO OFF
-dotnet restore %~dp0..\
-pushd %~dp0..
-tools\nuget.exe restore "src\Microsoft.Diagnostics.Correlation.Signing\Microsoft.Diagnostics.Correlation.Signing.csproj" -PackagesDirectory packages
-popd
--- tools/runtest.cmd
+++
@@ -1,4 +0,0 @@
-@ECHO OFF
-SET failed=0
-for /f %%a in ('dir %~dp0..\test\project.json /b /s ^| find /i "Test"') do dotnet test %%a || set failed=1
-EXIT /B %failed%
